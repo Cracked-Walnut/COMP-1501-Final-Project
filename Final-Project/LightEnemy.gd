9c69f170e997189782fc189a3f39efdb8772f547
Cracked-Walnut <maolud62@gmail.com>
2023-03-24 19:44:58 -0400

    Added Code for Enemy Movement

--- Final-Project/LightEnemy.gd
+++ Final-Project/LightEnemy.gd
@@ -1,14 +1,29 @@
-extends "res://Character.gd"
+# https://www.youtube.com/watch?v=WJfjnwxM0Do
 
+# extends "res://Character.gd"
+extends KinematicBody2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var speed = 20
+var motion = Vector2()
+var gravity = 20
+var direction = 1 # 1 = right, -1 = left
 
+func _physics_process(delta):
+	motion.y += gravity
+	motion.x += speed * direction
+	
+#	if is_on_wall():
+#		direction = direction * -1
+		
+	motion = move_and_slide(motion)
+	
+func is_on_wall():
+	print("Wall")
+	
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+#func _ready():
+#	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
